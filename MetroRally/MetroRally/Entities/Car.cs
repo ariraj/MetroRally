9387b5071ee05651350c5f23e17aa633cd7b0801
ariraj <arira@metropolia.fi>
2013-11-25 16:10:43 +0200

    First commit

---
+++ MetroRally/MetroRally/Entities/Car.cs
@@ -0,0 +1,19 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetroRally.Entities
+{
+    class Car
+    {
+        public Vector2 Position;
+
+
+        public Car()
+        {
+            Position = new Vector2(0,500);
+        }
+    }
+}
---
+++ MetroRally/MetroRally/Game1.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Devices.Sensors;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Media;
+using MetroRally.Entities;
+
+namespace MetroRally
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Car car;
+        Texture2D carTexture;
+        Motion motion;
+        
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            if (Motion.IsSupported)
+                motion = new Motion();
+
+            Content.RootDirectory = "Content";
+
+            // Frame rate is 30 fps by default for Windows Phone.
+            TargetElapsedTime = TimeSpan.FromTicks(333333);
+
+            // Extend battery life under lock.
+            InactiveSleepTime = TimeSpan.FromSeconds(1);
+            graphics.SupportedOrientations = DisplayOrientation.Portrait;
+            graphics.PreferredBackBufferHeight = 1280;
+            graphics.PreferredBackBufferWidth = 768;
+            graphics.IsFullScreen = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            car = new Car();
+            if (motion != null)
+            {
+                motion.CurrentValueChanged += new EventHandler<SensorReadingEventArgs<MotionReading>>(motion_CurrentValueChanged);
+                motion.Start();
+            }
+            
+            
+
+            base.Initialize();
+        }
+
+        void motion_CurrentValueChanged(object sender, SensorReadingEventArgs<MotionReading> e)
+        {
+
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            carTexture = this.Content.Load<Texture2D>(".\\Textures\\car");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+            TouchCollection currentTouch = TouchPanel.GetState();
+
+            foreach (TouchLocation location in currentTouch)
+            {
+                switch (location.State)
+                {
+                    case TouchLocationState.Pressed:
+                        car.Position.X = location.Position.X;
+                        //car.Position.Y = location.Position.Y;
+                        break;
+                    case TouchLocationState.Moved:
+                        car.Position.X = location.Position.X;
+                       // car.Position.Y = location.Position.Y;
+                        break;
+                }
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
+            spriteBatch.Draw(carTexture, car.Position, Color.White);
+            
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
